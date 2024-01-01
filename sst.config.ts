6c63024feaf20327357575d169b3eb9ee810705b
Martin Mueller <damadden88@googlemail.com>
2024-01-01 09:09:20 +0000

    add bucket and upload code

---
+++ sst.config.ts
@@ -0,0 +1,21 @@
+import { SSTConfig } from "sst";
+import { Bucket, NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "sst-nextjs-s3-picture-uploader",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const bucket = new Bucket(stack, "public");
+      const site = new NextjsSite(stack, "site");
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
